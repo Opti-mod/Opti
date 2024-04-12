08aec930f26afda03745b8d3e1f9afc2ebc84d9e
byeoon <47872200+byeoon@users.noreply.github.com>
2024-04-12 15:06:31 +0000

    please dont crash

--- src/lib/tweak.ts
+++ src/lib/tweak.ts
@@ -3,8 +3,15 @@ import { useProxy } from "@lib/storage";
 import { silentTyping, unloadSilentTyping } from "@/lib/tweak/silentTyping";
 import { hideDumbButtons, unloadHideButtons } from "@/lib/tweak/HideDumbButtons";
 import { trustURL, unloadTrustURL } from "@/lib/tweak/trustURL";
+import { ErrorBoundary } from "@/ui/components";
 
 export function initTweaks()
 {
     console.log("WTF?????????")
+    useProxy(settings);
+    settings.tweaks ??= {};
+    
+    return(
+        console.log("Wtf 2 electric boogaloo")
+    )
 }
