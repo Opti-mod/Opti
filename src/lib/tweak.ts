b7ad8aafc7a07e0db180071370eda4beba81b637
byron <47872200+byeoon@users.noreply.github.com>
2024-04-12 17:20:54 -0400

    push fix hopefully

--- src/lib/tweak.ts
+++ src/lib/tweak.ts
@@ -8,6 +8,21 @@ import { removeDeletePrompt, unloadRemoveDelete } from "./tweak/removeDelete";
 export function initTweaks()
 {
     console.log("TweakManager has initialized.");
+    if(settings.tweaks.trustURL == undefined)
+        settings.tweaks.trustURL = false;
+
+    if(settings.tweaks.hideButtons == undefined)
+        settings.tweaks.hideButtons = false;
+
+    if(settings.tweaks.silentTyping == undefined)
+        settings.tweaks.silentTyping = false;
+
+    if(settings.tweaks.experiments == undefined)
+        settings.tweaks.experiments = false;
+
+    if(settings.tweaks.fastdelete == undefined)
+        settings.tweaks.fastdelete = false;
+    
     (settings.tweaks.trustURL ? trustURL : unloadTrustURL)();
     (settings.tweaks.hideButtons ? hideDumbButtons : unloadHideButtons)();
     (settings.tweaks.silentTyping ? silentTyping : unloadSilentTyping)();
