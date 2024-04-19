5ce249fc23b62c635535d03489ff04fae6211335
byeoon <47872200+byeoon@users.noreply.github.com>
2024-04-19 13:46:11 +0000

    can someone help me with badge support??!?!

--- src/lib/badges.ts
+++ src/lib/badges.ts
@@ -1,6 +1,18 @@
 // please help.
 
+// opti developer
+// opti contributor
+// opti addon developer
+// opti supporter
+
 export function initBadges()
 {
   
 }
+
+
+function patchBadges()
+{
+    //TODO: render badges
+}
+
