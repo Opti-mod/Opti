1d8ca67ea5cbfb6e92cc8b3d6eca4709c5f39ca3
byron <47872200+byeoon@users.noreply.github.com>
2024-04-19 18:52:44 -0400

    actually functional debug command

--- src/lib/command/debug.tsx
+++ src/lib/command/debug.tsx
@@ -1,12 +1,19 @@
 import { ApplicationCommand } from "@/def";
 import { Messages } from "../metro/common";
+import { getDebugInfo } from "../debug";
 
+const debugInfo = getDebugInfo();
 export default [ 
     {
         name: 'debug',
         description: 'Prints Optis debug information to chat.',
         execute(_, ctx) {
-            const content = "Opti Debug Info: my balls itch rn hold on"
+            const content = `**Opti Debug Info > ** 
+            \nOpti Version: ${debugInfo.vendetta.version}\n
+            Discord Version: ${debugInfo.discord.version} (Build ${debugInfo.discord.build})\n
+            Device: ${debugInfo.device}\n
+            OS Name: ${debugInfo.os.name}
+            `
             Messages.sendMessage(ctx.channel.id, 
             {
                 content: content
