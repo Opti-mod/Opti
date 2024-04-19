15e280026aebf6b4e1beb1cb5cee8fcc2ae19af7
byron <47872200+byeoon@users.noreply.github.com>
2024-04-19 19:12:03 -0400

    added more debug info

--- src/lib/command/debug.tsx
+++ src/lib/command/debug.tsx
@@ -8,19 +8,18 @@ export default [
         name: 'debug',
         description: 'Prints Optis debug information to chat.',
         execute(_, ctx) {
-            const content = `** < Opti Debug Info > **
+        const content = `** < Opti Debug Info > **
 
-            **Opti Version**: ${debugInfo.vendetta.version}
-            **Discord Version**: ${debugInfo.discord.version} (Build ${debugInfo.discord.build})
-            **Device Brand**: ${debugInfo.device.brand}
-            **OS Name**: ${debugInfo.os.name} ${debugInfo.os.version}
-            Codename/Machine ID: ${debugInfo.device.codename}
-            `
-            Messages.sendMessage(ctx.channel.id, 
-            {
-                content: content
-            }
-            );
+        **Opti Version**: ${debugInfo.vendetta.version}
+        **Discord Version**: ${debugInfo.discord.version} (Build ${debugInfo.discord.build})
+        **Hermes**: ${debugInfo.hermes.version}
+        **Bytecode**: ${debugInfo.hermes.bytecodeVersion}
+
+        **Device Brand**: ${debugInfo.device.brand}
+        **OS**: ${debugInfo.os.name} ${debugInfo.os.version}
+        **Codename/Machine ID**: ${debugInfo.device.codename}`
+
+        Messages.sendMessage(ctx.channel.id, { content: content });
         },
     },
 ] as ApplicationCommand[]
