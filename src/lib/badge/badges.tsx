9f6e70ba52eb84b11b235a799a70652aeac10a41
byeoon <47872200+byeoon@users.noreply.github.com>
2024-04-22 16:51:16 +0000

    object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.object object is not a function in String, fuck you, now its gone.

--- src/lib/badge/badges.tsx
+++ src/lib/badge/badges.tsx
@@ -1,61 +1,14 @@
-/*
- YES I KNOW THIS IS FROM ALIUCORD AND I DO NOT PLAN ON KEEPING IT LIKE THIS
- THIS IS JUST A CONCEPT ON TESTING BADGES
-
-  YES I KNOW THIS IS FROM ALIUCORD AND I DO NOT PLAN ON KEEPING IT LIKE THIS
-  THIS IS JUST A CONCEPT ON TESTING BADGES
-
-  YES I KNOW THIS IS FROM ALIUCORD AND I DO NOT PLAN ON KEEPING IT LIKE THIS
- THIS IS JUST A CONCEPT ON TESTING BADGES
-
-  YES I KNOW THIS IS FROM ALIUCORD AND I DO NOT PLAN ON KEEPING IT LIKE THIS
- THIS IS JUST A CONCEPT ON TESTING BADGES
-*/
-
 import { after } from "../patcher";
 import { ReactNative as RN, React, stylesheet } from "../metro/common";
 import { findByName } from "../metro/filters";
 
-const { View, Image, TouchableOpacity } = RN;
 
-interface Badge {
-    url: string;
-    text: string;
-}
-
-const styles = stylesheet.createThemedStyleSheet({
-    container: {
-        flexDirection: "row",
-        alignItems: "center",
-        flexWrap: "wrap",
-        justifyContent: "flex-end",
-    },
-    img: {
-        width: 24,
-        height: 24,
-        resizeMode: "contain",
-        marginHorizontal: 4
-    }
-});
-
-const ProfileBadges = findByName("ProfileBadges", false);
 
 export function patchBadges()
 {
-
-    after(ProfileBadges, "default", (_, ctx) => {
-        const renderedBadgesView = (
-            <View key="opti-badges" style={styles.container}>
-                    <TouchableOpacity key={'https://raw.githubusercontent.com/Opti-mod/assets/main/opti_cog.PNG'} onPress={() => {
-                    }}>
-                        <Image source={{ uri: 'https://raw.githubusercontent.com/Opti-mod/assets/main/opti_cog.PNG' }} style={styles.img} />
-                    </TouchableOpacity>
-            </View>
-        );
-
-        if (!ctx.result) return renderedBadgesView;
-
-        ctx.result.props.children.push(renderedBadgesView);
-        return;
-    });
+    // here lies the attempts to port
+    // - aliucord badge implomentatoon method
+    // - enmity badge implomentation
+    // more to come. 
+    console.log("object object is not a function in String, fuck you, now its gone. no more strings to complain about.");
 }
