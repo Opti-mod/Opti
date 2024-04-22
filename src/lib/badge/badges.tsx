bd35054036400cb3ca2fed798da82d3bb6c908d1
byeoon <47872200+byeoon@users.noreply.github.com>
2024-04-22 16:53:45 +0000

    made code note better, hury up!

--- src/lib/badge/badges.tsx
+++ src/lib/badge/badges.tsx
@@ -1,14 +1,9 @@
-import { after } from "../patcher";
-import { ReactNative as RN, React, stylesheet } from "../metro/common";
-import { findByName } from "../metro/filters";
-
-
-
 export function patchBadges()
 {
-    // here lies the attempts to port
-    // - aliucord badge implomentatoon method
-    // - enmity badge implomentation
-    // more to come. 
+    // here lies the attempts to make badges by using the following implomentation methods:
+    // aliucord
+    // enmity
+    // 2x 3rd party plugins
+    // and more soon. 
     console.log("object object is not a function in String, fuck you, now its gone. no more strings to complain about.");
 }
