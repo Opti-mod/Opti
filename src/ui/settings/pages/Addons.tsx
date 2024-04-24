a6736f1a831aab28851666763e2efb2585b2d759
byeoon <47872200+byeoon@users.noreply.github.com>
2024-04-24 14:05:56 +0000

    test

--- src/ui/settings/pages/Addons.tsx
+++ src/ui/settings/pages/Addons.tsx
@@ -5,20 +5,31 @@ import { themes } from "@lib/themes";
 import settings from "@lib/settings";
 import AddonPage from "@ui/settings/components/AddonPage";
 import PluginCard from "@ui/settings/components/PluginCard";
-import { ErrorBoundary } from "@/ui/components";
+import { Button, ErrorBoundary } from "@/ui/components";
 import { ReactNative as RN } from "@metro/common";
 import ThemeCard from "../components/ThemeCard";
 
 export default function Addons() {
     useProxy(settings)
+    let plugin = true;
 
     return (
+        
         <ErrorBoundary>
         <RN.View style={{ flex: 1 }}>
-        <AddonPage<Plugin>
+        <Button
+                    color={Button.Colors.BRAND}
+                    size={Button.Sizes.MEDIUM}
+                    look={Button.Looks.FILLED}
+                    onPress={() => console.log("hi")}
+                    text="Switch"
+                />
+
+        {plugin &&
+            <AddonPage<Plugin> 
             items={plugins}
             card={PluginCard}
-        />
+        />}
 
         <AddonPage<Theme>
         items={themes}
