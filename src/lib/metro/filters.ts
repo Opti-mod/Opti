358253a7797e1c0cc723fba2a96c634c97b8e842
byeoon <47872200+byeoon@users.noreply.github.com>
2024-10-15 14:17:14 +0000

    very limited to my smarts

--- src/lib/metro/filters.ts
+++ src/lib/metro/filters.ts
@@ -94,6 +94,7 @@ const storeFilter = (name: string) => (m: any) => m.getName && m.getName.length 
 
 export const findByProps: PropsFinder = (...props) => find(propsFilter(props));
 export const findByPropsAll: PropsFinderAll = (...props) => findAll(propsFilter(props));
+
 export const findByName = (name: string, defaultExp = true) => find(nameFilter(name, defaultExp));
 export const findByNameAll = (name: string, defaultExp = true) => findAll(nameFilter(name, defaultExp));
 export const findByDisplayName = (displayName: string, defaultExp = true) => find(dNameFilter(displayName, defaultExp));
