af49f2d7daee69c093888b310d7a3b9f2c8cde5b
byeoon <47872200+byeoon@users.noreply.github.com>
2024-10-16 15:41:20 +0000

    test again

--- src/entry.ts
+++ src/entry.ts
@@ -1,4 +1,6 @@
 import { ClientInfoManager } from "@lib/native";
+import { NavigationNative } from "./lib/metro/common";
+import Developer from "./ui/settings/pages/Developer";
 
 // This logs in the native logging implementation, e.g. logcat
 console.log("Hello from Opti!");
@@ -6,11 +8,16 @@ console.log("Hello from Opti!");
 // Make 'freeze' and 'seal' do nothing
 Object.freeze = Object;
 Object.seal = Object;
+const navigation = NavigationNative.useNavigation();
 
 import(".").then((m) => m.default()).catch((e) => {
     console.log(e?.stack ?? e.toString());
+    navigation.push("VendettaCustomPage", {
+        title: "Welcome to Opti",
+        render: Developer,
+    });
     alert([
-        "Failed to load Vendetta!\n",
+        "Failed to load Opti!\n",
         `Build Number: ${ClientInfoManager.Build}`,
         `Vendetta: ${__optiVersion}`,
         e?.stack || e.toString(),
--- src/lib/security.ts
+++ src/lib/security.ts
@@ -4,9 +4,5 @@ import { ReactNative as RN, NavigationNative } from "@metro/common";
 
 export function patchSecurity() {
     const navigation = NavigationNative.useNavigation();
-
-    navigation.push("VendettaCustomPage", {
-        title: "Welcome to Opti",
-        render: Developer,
-    });
+    // hm.
 }
