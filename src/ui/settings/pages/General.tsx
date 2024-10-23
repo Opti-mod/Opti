6da20c7e2a6e238051f223e1d2ee413aa068fb50
byeoon <47872200+byeoon@users.noreply.github.com>
2024-10-23 13:14:03 +0000

    fixed icons

--- src/ui/settings/pages/General.tsx
+++ src/ui/settings/pages/General.tsx
@@ -20,33 +20,34 @@ export default function General() {
         <ErrorBoundary>
                 <RN.ScrollView style={{ flex: 1 }} contentContainerStyle={{ padding: 24, alignItems: "center" }}>
                 <Stack spacing={16}>
+                <TableRowGroup title="Actions">
+                    <TableRow
+                        label="Reload Discord"
+                        icon={<TableRowIcon source={getAssetIDByName("ic_message_retry")} />}
+                        description="This may crash your client, use at your own risk!"
+                        onPress={() => BundleUpdaterManager.reload()}
+                    />
+                    <TableSwitchRow
+                        label="Developer Settings"
+                        leading={<TableRowIcon source={getAssetIDByName("ic_progress_wrench_24px")} />}
+                        value={settings.developerSettings}
+                        onValueChange={(v: boolean) => {
+                            settings.developerSettings = v;
+                        }}
+                    />
+                </TableRowGroup>
                 <TableRowGroup title="Links" titleStyleType="no_border">
                     <TableRow
                         label="Discord Server"
-                        leading={<FormRow.Icon source={getAssetIDByName("Discord")} />}
+                        leading={<TableRowIcon source={getAssetIDByName("Discord")} />}
                         trailing={FormRow.Arrow}
                         onPress={() => url.openDeeplink(DISCORD_SERVER)}
                     />
                     <TableRow
                         label="GitHub"
-                        leading={<FormRow.Icon source={getAssetIDByName("img_account_sync_github_white")} />}
+                        leading={<TableRowIcon source={getAssetIDByName("img_account_sync_github_white")} />}
                         trailing={FormRow.Arrow}
                         onPress={() => url.openURL(GITHUB)}
-                    />
-                </TableRowGroup>
-                <TableRowGroup title="Actions">
-                    <TableRow
-                        label="Reload Discord"
-                        icon={<TableRowIcon source={getAssetIDByName("ic_message_retry")} />}
-                        onPress={() => BundleUpdaterManager.reload()}
-                    />
-                    <TableSwitchRow
-                        label="Developer Settings"
-                        leading={<FormRow.Icon source={getAssetIDByName("ic_progress_wrench_24px")} />}
-                        value={settings.developerSettings}
-                        onValueChange={(v: boolean) => {
-                            settings.developerSettings = v;
-                        }}
                     />
                 </TableRowGroup>
                 </Stack>
